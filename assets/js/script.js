3f09ae0ce949f3e15eeeaa704659299b442d0c60
Sureshot0341 <gregpetropoulos@yahoo.com>
2021-02-13 22:19:30 -0500

    Set up the frame for index.html, results.html, stytesheet, and js.script, most fo the styling and html is finished. I will spend the remainder on writing the javascript

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//pseudo code
+//set up timer function to count down and to subtract -10seconds from every wrong answer
+//start quiz button will start the timer and iterate through multiple choice questions
+//multiple choice questions are buttons and 4 of them
+//ask for intitials and record the results on the second page 
+
+var counter = 
